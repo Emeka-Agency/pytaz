5460fd4e5cfdf539ab3db9cea8d5bb75917dafb6
kratoar <mael.fallet@gmail.com>
2023-05-22 13:32:56 +0200

    Uniformisation URL de fetch

--- api_test.py
+++ api_test.py
@@ -80,4 +80,4 @@ async def get_keywords(query: str, gl: str = "fr", hl: str = "fr", nb_keywords: 
         )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=1234)
+    uvicorn.run(app, host="0.0.0.0", port=7777)
