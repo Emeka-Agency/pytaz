dc11fc8d90db358eb08b9dfc3ecd85170416049e
kratoar <mael.fallet@gmail.com>
2023-06-27 14:36:50 +0200

    Ajout de stopwords

--- stopwords_list.py
+++ stopwords_list.py
@@ -4,8 +4,25 @@ nltk.download('punkt')
 
 from nltk.corpus import stopwords
 
-STOPWORDS_FILE = 'stopwords_list.txt'
-stop_words = set(stopwords.words('french'))
-with open(STOPWORDS_FILE, 'r', encoding="UTF-8") as f:
-    additional_stopwords = [line.strip() for line in f]
-stop_words.update(additional_stopwords)
+# http://members.unine.ch/jacques.savoy/clef/index.html
+
+stop_words = []
+
+STOPWORDS_FILES = [
+    'stopwords_list.txt',
+    'complement_stopwords.txt',
+    'savoy_fr.txt',
+    'savoy_en.txt',
+]
+
+for file in STOPWORDS_FILES:
+    with open(file, 'r', encoding="UTF-8") as f:
+        for line in f:
+            stop_words.append(line.strip())
+
+languages = ["french", "english"]
+
+for language in languages:
+    stops = stopwords.words(language)
+    for stop in stops:
+        stop_words.append(stop.strip())
